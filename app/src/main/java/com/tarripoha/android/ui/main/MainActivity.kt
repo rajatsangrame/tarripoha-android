9a25ed2e362d60ea73e14a3bd1aa7ca510db1297
rajatsangrame <rajatsangrame@gmail.com>
2021-03-19 00:18:18 +0530

    serach icon in homescreen

--- app/src/main/java/com/tarripoha/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/tarripoha/android/ui/main/MainActivity.kt
@@ -2,12 +2,16 @@ package com.tarripoha.android.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.lifecycle.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tarripoha.android.App
+import com.tarripoha.android.R
 import com.tarripoha.android.data.db.Word
 import com.tarripoha.android.databinding.ActivityMainBinding
 import com.tarripoha.android.di.component.DaggerMainActivityComponent
@@ -21,145 +25,174 @@ import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
 
-  @Inject
-  lateinit var factory: ViewModelFactory
+    @Inject
+    lateinit var factory: ViewModelFactory
 
-  private lateinit var viewModel: MainViewModel
-  private lateinit var binding: ActivityMainBinding
-  private lateinit var wordAdapter: WordAdapter
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-    getDependency()
-    viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
+    // region Activity Related
 
-    init()
-    fetchAllWord()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getDependency()
+        viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
 
-  private fun init() {
-    setUpRecycleView()
-    setUpListeners()
-    setupObservers()
-  }
+        setupUI()
+        fetchAllWord()
+    }
 
-  private fun fetchAllWord() {
-    viewModel.fetchAllWord()
-  }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
 
-  private fun setUpRecycleView() {
-    val linearLayoutManager = LinearLayoutManager(
-        this, RecyclerView.VERTICAL, false
-    )
-    binding.layout.withSwipeRv.layoutManager = linearLayoutManager
-    binding.layout.withSwipeRv.isNestedScrollingEnabled = false
-    wordAdapter = WordAdapter(ArrayList(), object : ItemClickListener<Word> {
-      override fun onClick(
-        position: Int,
-        data: Word
-      ) {
-        val intent = Intent(this@MainActivity, WordActivity::class.java)
-        intent.putExtra(WordActivity.KEY_WORD, data)
-        startActivityForResult(intent, REQUEST_CODE_WORD)
-      }
-    })
-    binding.layout.withSwipeRv.adapter = wordAdapter
-    binding.layout.withSwipeRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-      override fun onScrolled(
-        recyclerView: RecyclerView,
-        dx: Int,
-        dy: Int
-      ) {
-        val position = linearLayoutManager.findFirstCompletelyVisibleItemPosition()
-        binding.layout.swipeRefreshLayout.isEnabled = position <= 0
-      }
-    })
-    val callback = Callback()
-    val itemTouchHelper = ItemTouchHelper(callback)
-    //itemTouchHelper.attachToRecyclerView(with_swipe_rv)
-  }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.search -> {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_toolbar_back)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
-  private fun setupObservers() {
-    viewModel.isRefreshing()
-        .observe(this, Observer {
-          it.let {
-            binding.layout.swipeRefreshLayout.isRefreshing = it
-          }
-        })
-    viewModel.getAllWords()
-        .observe(this, Observer {
-          it?.let {
-            wordAdapter.setWordList(it)
-          }
-        })
-    viewModel.getUserMessage()
-        .observe(this, Observer {
-          TPUtils.showSnacBar(this, it)
-        })
-  }
+    //endregion
 
-  private fun getDependency() {
-    val component: MainActivityComponent = DaggerMainActivityComponent
-        .builder()
-        .applicationComponent(
-            App.get(this)
-                .getComponent()
+    private fun setupUI() {
+        setupToolbar()
+        setUpRecycleView()
+        setUpListeners()
+        setupObservers()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbarLayout.toolbar)
+        supportActionBar?.title = null
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+    }
+
+    private fun fetchAllWord() {
+        viewModel.fetchAllWord()
+    }
+
+    private fun setUpRecycleView() {
+        val linearLayoutManager = LinearLayoutManager(
+            this, RecyclerView.VERTICAL, false
         )
-        .build()
-    component.injectMainActivity(this)
-  }
-
-  private fun setUpListeners() {
-    binding.btnAdd.setOnClickListener {
-      startActivityForResult(Intent(this, WordActivity::class.java), REQUEST_CODE_WORD)
-    }
-    binding.layout.swipeRefreshLayout.setOnRefreshListener {
-      fetchAllWord()
-    }
-  }
-
-  override fun onActivityResult(
-    requestCode: Int,
-    resultCode: Int,
-    data: Intent?
-  ) {
-    if (requestCode == REQUEST_CODE_WORD && resultCode == RESULT_OK) {
-      val word = data?.getParcelableExtra<Word>(WordActivity.KEY_WORD)
-      if (word is Word) {
-        viewModel.addWord(word)
-      }
-    }
-    super.onActivityResult(requestCode, resultCode, data)
-  }
-
-  inner class Callback : ItemTouchHelper.Callback() {
-
-    override fun getMovementFlags(
-      recyclerView: RecyclerView,
-      viewHolder: RecyclerView.ViewHolder
-    ): Int {
-      return makeMovementFlags(0, ItemTouchHelper.RIGHT)
+        binding.layout.withSwipeRv.layoutManager = linearLayoutManager
+        binding.layout.withSwipeRv.isNestedScrollingEnabled = false
+        wordAdapter = WordAdapter(ArrayList(), object : ItemClickListener<Word> {
+            override fun onClick(
+                position: Int,
+                data: Word
+            ) {
+                val intent = Intent(this@MainActivity, WordActivity::class.java)
+                intent.putExtra(WordActivity.KEY_WORD, data)
+                startActivityForResult(intent, REQUEST_CODE_WORD)
+            }
+        })
+        binding.layout.withSwipeRv.adapter = wordAdapter
+        binding.layout.withSwipeRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(
+                recyclerView: RecyclerView,
+                dx: Int,
+                dy: Int
+            ) {
+                val position = linearLayoutManager.findFirstCompletelyVisibleItemPosition()
+                binding.layout.swipeRefreshLayout.isEnabled = position <= 0
+            }
+        })
+        val callback = Callback()
+        val itemTouchHelper = ItemTouchHelper(callback)
+        //itemTouchHelper.attachToRecyclerView(with_swipe_rv)
     }
 
-    override fun onMove(
-      recyclerView: RecyclerView,
-      viewHolder: RecyclerView.ViewHolder,
-      target: RecyclerView.ViewHolder
-    ): Boolean {
-      return false
+    private fun setupObservers() {
+        viewModel.isRefreshing()
+            .observe(this, Observer {
+                it.let {
+                    binding.layout.swipeRefreshLayout.isRefreshing = it
+                }
+            })
+        viewModel.getAllWords()
+            .observe(this, Observer {
+                it?.let {
+                    wordAdapter.setWordList(it)
+                }
+            })
+        viewModel.getUserMessage()
+            .observe(this, Observer {
+                TPUtils.showSnacBar(this, it)
+            })
     }
 
-    override fun onSwiped(
-      viewHolder: RecyclerView.ViewHolder,
-      direction: Int
+    private fun getDependency() {
+        val component: MainActivityComponent = DaggerMainActivityComponent
+            .builder()
+            .applicationComponent(
+                App.get(this)
+                    .getComponent()
+            )
+            .build()
+        component.injectMainActivity(this)
+    }
+
+    private fun setUpListeners() {
+        binding.btnAdd.setOnClickListener {
+            startActivityForResult(Intent(this, WordActivity::class.java), REQUEST_CODE_WORD)
+        }
+        binding.layout.swipeRefreshLayout.setOnRefreshListener {
+            fetchAllWord()
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
     ) {
+        if (requestCode == REQUEST_CODE_WORD && resultCode == RESULT_OK) {
+            val word = data?.getParcelableExtra<Word>(WordActivity.KEY_WORD)
+            if (word is Word) {
+                viewModel.addWord(word)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
-  }
 
-  companion object {
-    private const val TAG = "MainActivity"
-    private const val REQUEST_CODE_WORD = 101
-  }
+    inner class Callback : ItemTouchHelper.Callback() {
+
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            return makeMovementFlags(0, ItemTouchHelper.RIGHT)
+        }
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(
+            viewHolder: RecyclerView.ViewHolder,
+            direction: Int
+        ) {
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val REQUEST_CODE_WORD = 101
+    }
 }
